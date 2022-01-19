46294cc3b98e72b20f6bcf23f629326969d0b7cd
Ole994 <69842859+Ole994@users.noreply.github.com>
2022-01-19 14:44:16 +0100

    a problem width navigationbar fixed
    The problem was that when i clicked on the mouse, everything disappeared with this code:
     .navigation:hover,
     :active {
     width: 300px;
     }
    I still have not figured out how the navbar will open when hovering so that I can see rest of the content

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,13 @@
 
-const navigation = document.querySelector(".navigation");
+// const navigation = document.querySelector(".navigation");
 
-navigation.addEventListener("click", () => {
+// navigation.addEventListener("click", () => {
+// });
 
-});
-
-
+function toggleMenu() {
+    let navigation = document.querySelector(".navigation");
+    navigation.classList.toggle("active");
+    navigation.classList.toggle("closed");
+}
 
 

25dfbbd4c41ff91de72bac65e9f3353c25826cb6
Ole994 <69842859+Ole994@users.noreply.github.com>
2022-01-20 15:01:14 +0100

    hamburgermenu in progress
    Something works, but I dont know what I will do with it et

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,10 +4,20 @@
 // navigation.addEventListener("click", () => {
 // });
 
-function toggleMenu() {
-    let navigation = document.querySelector(".navigation");
-    navigation.classList.toggle("active");
-    navigation.classList.toggle("closed");
-}
+// function toggleMenu() {
+//     let navigation = document.querySelector(".navigation");
+//     navigation.classList.toggle("active");
+//     navigation.classList.toggle("closed");
+// }
+// const navigation = document.querySelector(".navigation");
+// const hamburger = document.querySelector(".toggle");
+
+// hamburger.addEventListener("click", () => {
+//     navigation.classList.toggle("open");
+//     // links.forEach(link => {
+//     //     link.classList.toggle("fade");
+//     // });log
+//     console.log(navigation);
+// });
 
 

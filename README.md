dc0214f26040dfb4c6d93d0285178f06038d5670
Ole994 <69842859+Ole994@users.noreply.github.com>
2022-01-24 18:49:23 +0100

    almost completed the page

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # portfolio2022
 
-sass --watch ./sass/main.scss ./css/output.css
+This is a webpage where you can have a look at who Ole is, contact information about him and what projects that he have made
--- js/hamburger.js
+++ js/hamburger.js
@@ -1,7 +1,5 @@
-
 const navigation = document.querySelector(".navigation");
 const menu = document.querySelector(".toggle");
-
 menu.addEventListener("click", () => {
     navigation.classList.toggle('open');
 })
